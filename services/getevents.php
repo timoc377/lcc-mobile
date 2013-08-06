cdc98b0795328b6d9951a21190951adb0a8b5800
Tim Carrington <timoc377@gmail.com>
2013-08-06 08:42:49 -0400

    event date sorting
    previously sorted by ID, corrected defect to sort properly by start date

--- services/getevents.php
+++ services/getevents.php
@@ -6,14 +6,14 @@ header('Access-Control-Allow-Origin: *');
 
 include 'config.php';
 
-$sql = "SELECT jee.id, IFNULL(jev.venue,'') AS venue, jec.catname, IFNULL(DATE_FORMAT(jee.dates,'%b %e'),'') AS dates, IFNULL(DATE_FORMAT(jee.enddates,'%b %e'),'') AS enddates, IFNULL(TIME_FORMAT(jee.times,'%l:%i %p'),'') AS times, IFNULL(TIME_FORMAT(jee.endtimes,'%l:%i %p'),'') AS endtimes, jee.title, jee.datdescription " . 
+$sql = "SELECT jee.id, IFNULL(jev.venue,'') AS venue, jec.catname, jee.dates AS sortDates, IFNULL(DATE_FORMAT(jee.dates,'%b %e'),'') AS dates, IFNULL(DATE_FORMAT(jee.enddates,'%b %e'),'') AS enddates, IFNULL(TIME_FORMAT(jee.times,'%l:%i %p'),'') AS times, IFNULL(TIME_FORMAT(jee.endtimes,'%l:%i %p'),'') AS endtimes, jee.title, jee.datdescription " . 
 		"FROM jos_eventlist_events jee " . 
 			"LEFT OUTER JOIN jos_eventlist_venues jev " . 
   				"ON jee.locid = jev.id " . 
 			"LEFT OUTER JOIN jos_eventlist_categories jec " . 
   				"ON jee.catsid = jec.id " .
 		"WHERE jee.published = 1 " .
-		"ORDER BY id";
+		"ORDER BY sortDates";
 
 try {
 	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass,array(
