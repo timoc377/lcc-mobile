5b65e82a8b96dfdec4354022077e502145a9e4b1
Tim Carrington <timoc377@gmail.com>
2014-02-06 12:51:03 -0500

    updated events services
    pointed event list and event details services to new site, which is
    currently under construction

--- services/config.php
+++ services/config.php
@@ -1,8 +1,8 @@
 <?php
 	
 	$dbhost = 'xdbs1.dailyrazor.com';
-	$dbuser = 'inspire_lighthou';
-	$dbpass = 'K1ngd0mM1nd3d';
-	$dbname = 'inspire_lighthousejoom';
+	$dbuser = 'inspire_lccjoom3';
+	$dbpass = '$K1ngd0mM1nd3d';
+	$dbname = 'inspire_lccjoomla3';
 
 ?>
--- services/getevents.php
+++ services/getevents.php
@@ -6,14 +6,14 @@ header('Access-Control-Allow-Origin: *');
 
 include 'config.php';
 
-$sql = "SELECT jee.id, IFNULL(jev.venue,'') AS venue, jec.catname, jee.dates AS sortDates, IFNULL(DATE_FORMAT(jee.dates,'%b %e'),'') AS dates, IFNULL(DATE_FORMAT(jee.enddates,'%b %e'),'') AS enddates, IFNULL(TIME_FORMAT(jee.times,'%l:%i %p'),'') AS times, IFNULL(TIME_FORMAT(jee.endtimes,'%l:%i %p'),'') AS endtimes, jee.title, jee.datdescription " . 
-		"FROM jos_eventlist_events jee " . 
-			"LEFT OUTER JOIN jos_eventlist_venues jev " . 
-  				"ON jee.locid = jev.id " . 
-			"LEFT OUTER JOIN jos_eventlist_categories jec " . 
-  				"ON jee.catsid = jec.id " .
-		"WHERE jee.published = 1 " .
-		"ORDER BY sortDates";
+$sql = "SELECT detail.evdet_id AS id, IFNULL(detail.summary,'') AS title, IFNULL(DATE_FORMAT(repetition.startrepeat,'%b %e'),'') AS dates, IFNULL(DATE_FORMAT(repetition.endrepeat,'%b %e'),'') AS enddates, IFNULL(TIME_FORMAT(repetition.startrepeat,'%l:%i %p'),'') AS times, IFNULL(TIME_FORMAT(repetition.endrepeat,'%l:%i %p'),'') AS endtimes, detail.location, detail.extra_info AS datdescription, ics.label " . 
+		"FROM z7njt_jevents_vevdetail detail JOIN z7njt_jevents_repetition repetition " . 
+			"ON detail.evdet_id = repetition.eventid " . 
+  			"JOIN z7njt_jevents_vevent event ON detail.evdet_id = event.ev_id " . 
+			"JOIN z7njt_jevents_icsfile ics ON ics.ics_id = event.icsid " . 
+  		"WHERE repetition.startrepeat >= curdate() " .
+		"AND detail.state = 1 " .
+		"ORDER BY repetition.startrepeat";
 
 try {
 	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass,array(
