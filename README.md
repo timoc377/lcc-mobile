ccb242ac5f81fcd9dc04804bfe7ccceb32457464
nathanstanford2 <nathan.stanford2@gmail.com>
2014-02-09 19:57:12 -0500

    creates the new cordova project

--- README.md
+++
@@ -1,7 +0,0 @@
-#Lighthouse Mobile Connect
-
-
-Lighthouse Mobile Connect is a notifier that will keep you informed and up to date 
-on what is going on with Lighthouse Christian Center in Milford, DE at all times. 
-
-##By: Tim Carrington and Nathan Stanford
---
+++ platforms/ios/www/js/adapters/jsonp-adapter.js
@@ -0,0 +1,34 @@
+var JSONPAdapter = function() {
+
+    this.initialize = function(data) {
+	url = typeof data !== 'undefined' ? data : "http://m.go2lighthouse.org";
+	var deferred = $.Deferred();
+	deferred.resolve();
+	return deferred.promise();
+    }
+
+    /* Get all service requests */
+    this.eventsList = function(id) {
+	return $.ajax({url: url + '/services/getevents.php', dataType: "json"});
+    }
+    this.prayersList = function(id) {
+	return $.ajax({url: url + '/v2/services/getprayerrequests.php', dataType: "json"});
+    }
+    this.noticesList = function(id) {
+	return $.ajax({url: url + '/v2/services/getnotices.php', dataType: "json"});
+    }
+    /* End Get all service requests*/
+
+    /* Find by id service requests */
+    this.findEventById = function(id) {
+	return $.ajax({url: url + "/services/getevent.php?id=" + id, dataType: "json"});
+    }
+    this.findPrayerById = function(id) {
+	return $.ajax({url: url + "/v2/services/getprayer.php?id=" + id, dataType: "json"});
+    }
+    this.findNoticeById = function(id) {
+	return $.ajax({url: url + "/v2/services/getnotice.php?id=" + id, dataType: "json"});
+    }
+    /* End find by id service requests */
+
+}
